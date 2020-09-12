b6cb9951ed8a63d95d4dd1e8e9d1a46bb92879b4
husen <husenshikalgar007@gmail.com>
2020-09-12 10:03:06 +0530

    updated custom validator

--- studentAng/src/app/registration/registration.component.ts
+++ studentAng/src/app/registration/registration.component.ts
@@ -1,13 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { StudentService } from '../_httpservice/student.service';
+import { CustomValidator } from '../shared/validators/custome.validator';
 import { ToastrService } from 'ngx-toastr';
 declare var $: any;
-
-// Custom Validators
-import { checkString } from '../shared/validators/string.validator';
-import { onlyNumber } from '../shared/validators/number.validator';
-
 
 @Component({
   selector: 'app-registration',
@@ -24,16 +20,16 @@ export class RegistrationComponent implements OnInit {
 
   ngOnInit(): void {
     this.studentFrm = this.fb.group({
-      first_name: ['', [Validators.required, checkString]],
-      last_name: ['', [Validators.required, checkString]],
+      first_name: ['', [Validators.required, CustomValidator.string]],
+      last_name: ['', [Validators.required, CustomValidator.string]],
       email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
-      pocket_money: ['', [Validators.required, onlyNumber]],
+      pocket_money: ['', [Validators.required, CustomValidator.numeric]],
       password: ['', Validators.required],
-      age: ['', [Validators.required, onlyNumber]],
-      city: ['', [Validators.required, checkString]],
-      state: ['', [Validators.required, checkString]],
-      zip: ['', [Validators.required, onlyNumber]],
-      country: ['', [Validators.required, checkString]],
+      age: ['', [Validators.required, CustomValidator.numeric]],
+      city: ['', [Validators.required, CustomValidator.string]],
+      state: ['', [Validators.required, CustomValidator.string]],
+      zip: ['', [Validators.required, CustomValidator.numeric]],
+      country: ['', [Validators.required, CustomValidator.string]],
     });
   }
 
@@ -45,38 +41,25 @@ export class RegistrationComponent implements OnInit {
     }
 
     // Created formData Object and appended all the input values
-    var fd = new FormData();
-    fd.append('first_name', this.studentFrm.get('first_name').value);
-    fd.append('last_name', this.studentFrm.get('last_name').value);
-    fd.append('email', this.studentFrm.get('email').value);
-    fd.append('password', this.studentFrm.get('password').value);
-    fd.append('pocket_money', this.studentFrm.get('pocket_money').value);
-    fd.append('age', this.studentFrm.get('age').value);
-    fd.append('city', this.studentFrm.get('city').value);
-    fd.append('state', this.studentFrm.get('state').value);
-    fd.append('zip', this.studentFrm.get('zip').value);
-    fd.append('country', this.studentFrm.get('country').value);
-
+    var fd = this.convertFormData(this.studentFrm.value);
     this.std.addStudent(fd).subscribe((data) => {
-      if (data.status == false) {
+      if (data.status === false) {
         if ($.isEmptyObject(data.error_data)) {
           this.errorBlock = false;
           this.toastr.success('inserted successfully', 'Success!');
           this.onReset();
         } else {
           this.errorBlock = true;
-          $(".print-error-msg").css('display', 'block');
-          $(".print-error-msg").html(data.error_data);
+          $('.print-error-msg').css('display', 'block');
+          $('.print-error-msg').html(data.error_data);
         }
       } else {
         this.errorBlock = false;
         this.toastr.success('inserted successfully', 'Success!');
         this.onReset();
       }
-
     });
   }
-
 
   // convenience getter for easy access to form fields
   get f() { return this.studentFrm.controls; }
@@ -86,9 +69,15 @@ export class RegistrationComponent implements OnInit {
     this.submitted = false;
     this.errorBlock = false;
     this.studentFrm.reset();
-    $(".print-error-msg").css('display', 'none');
-    $(".print-error-msg").html('');
+    $('.print-error-msg').css('display', 'none');
+    $('.print-error-msg').html('');
+  }
 
+  /* Converter object into Form Data */
+  convertFormData(object: any) {
+    const formData = new FormData();
+    Object.keys(object).forEach(key => formData.append(key, object[key]));
+    return formData;
   }
 
 }
